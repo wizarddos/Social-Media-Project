b899bd8d617a348d6c0b67b3addbc24e8cecd4d2
wizarddos <wizarddos@github.com>
2021-02-25 15:19:31 +0100

    feature- Kilka nowych plików

---
+++ includes/connect.php
@@ -0,0 +1,4 @@
+<?php
+$db_dsn = "mysql:host=localhost;dbname=Social-Media";
+$db_user = "root";
+$db_pass = "";
---
+++ views/homepages/loged.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../../styles/styles.css"/>
+        <script  src="../../scripts/js/sidenav.js"></script>
+    </head>
+    <body>
+        <header>
+        <div id="mySidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+        </div>
+            <button onclick="openNav()">Click me</button>
+            <h1>HEllo World</h1>
+        </header>
+    </body>
+</html>
